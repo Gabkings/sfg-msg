20c255b1f10517cc050008a51c6c5d554e3d227a
Gabriel Gitonga <gabriel.gitonga@bskglobaltech.com>
2021-06-04 03:47:00 +0300

    Intial setup

---
+++ src/test/java/com/gabkings/sfgmsg/SfgMsgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabkings.sfgmsg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgMsgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
